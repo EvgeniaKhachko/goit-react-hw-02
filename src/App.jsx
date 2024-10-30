e0da00416236cdb940a214627cc575be1f86eafe
kapitu <ketkap007@gmail.com>
2024-10-30 23:50:19 +0200

    setings

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import 'modern-normalize';
+import 'nanoid';
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
