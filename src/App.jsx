aa1f6cec97db6dfbb24be467b9d6e0a8acfbdc92
kapitu <ketkap007@gmail.com>
2024-10-31 21:46:22 +0200

    hw-2-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'modern-normalize';
 import 'nanoid';
-import Description from './components/Description/Description';
+import Notification from './components/Notification/Notification';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import reactLogo from './assets/react.svg';
@@ -9,9 +9,69 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-  return <></>;
+  useEffect(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    if (savedFeedback) {
+      setFeedback(JSON.parse(savedFeedback));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+  // const totalFeedback = Object.values(feedback).reduce(
+  //   (acc, value) => acc + value,
+  //   0
+  // );
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback collected yet." />
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Options.module.css';
+
+function Options({ updateFeedback, resetFeedback, totalFeedback }) {
+  return (
+    <div className={s.button}>
+      <button onClick={() => updateFeedback('good')}>Good</button>
+      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
+      <button onClick={() => updateFeedback('bad')}>Bad</button>
+      {totalFeedback > 0 && (
+        <button onClick={() => resetFeedback('reset')}>Reset</button>
+      )}
+    </div>
+  );
+}
+
+export default Options;
